cbae114c2d049963a6faba0c43ad2fe4d518c6b0
martimd96 <martim.costa.duarte05@gmail.com>
2025-09-24 11:18:05 +0000

    progress1

--- RPS.py
+++ RPS.py
@@ -3,8 +3,21 @@
 def player(prev_play, opponent_history=[]):
     opponent_history.append(prev_play)
 
+    quincy_pattern = ['R', 'R', 'P', 'P', 'S']
+    ideal_plays = {'P': 'S', 'R': 'P', 'S': 'R'} 
+
     guess = "R"
-    if len(opponent_history) > 2:
-        guess = opponent_history[-2]
+    if len(opponent_history) > 4 and opponent_history[:5] == quincy_pattern:
+        last_two = opponent_history[-2:]
+        if last_two == ['P', 'P']:
+            guess = ideal_plays['S']
+        elif last_two == ['R', 'R']:
+            guess = ideal_plays['P']
+        elif last_two == ['P', 'S']:
+            guess = ideal_plays['R']
+        else:
+            guess = ideal_plays[opponent_history[-1]]
+
+       
 
     return guess
--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from RPS import player
 from unittest import main
 
 play(player, quincy, 1000)
-play(player, abbey, 1000)
-play(player, kris, 1000)
-play(player, mrugesh, 1000)
+#play(player, abbey, 1000)
+#play(player, kris, 1000)
+#play(player, mrugesh, 1000)
 
 # Uncomment line below to play interactively against a bot:
 # play(human, abbey, 20, verbose=True)
