17fbb2fe04480ed982c9b8c0dc1511e9e91658e9
martimd96 <martim.costa.duarte05@gmail.com>
2025-09-26 13:29:33 +0000

    DONE

--- RPS.py
+++ RPS.py
@@ -6,11 +6,11 @@ def player(prev_play, opponent_history=[]):
     quincy_pattern = ['R', 'R', 'P', 'P', 'S']
     kris_pattern = ['P', 'P', 'S', 'R', 'P']
     mrugesh_pattern = ['R', 'R', 'R', 'R', 'P']
+    abbey_pattern = ['P', 'P', 'P', 'P', 'P']
     ideal_plays = {'P': 'S', 'R': 'P', 'S': 'R'} 
     moves = ['R', 'P', 'S']
 
-    print(opponent_history[0:20])
-    guess = moves[(len(opponent_history) - 1) % 3]
+    #print(opponent_history[0:30])
     if len(opponent_history) > 10 and (opponent_history[5:10] == quincy_pattern):
         last_two = opponent_history[-2:]
         if last_two == ['P', 'P']:
@@ -25,11 +25,20 @@ def player(prev_play, opponent_history=[]):
                 guess = 'P'  # Default move for first turn
             else:
                 guess = ideal_plays[last_move]
-    elif len(opponent_history) > 1 and (opponent_history[1:6] == kris_pattern):
+    elif len(opponent_history) > 5 and (opponent_history[1:6] == kris_pattern):
         moves2 = ['S', 'P', 'R']
         guess = moves2[(len(opponent_history) - 1) % 3]
-    elif len(opponent_history) > 1 and (opponent_history[1:6] == mrugesh_pattern):
+    elif len(opponent_history) > 5 and (opponent_history[1:6] == mrugesh_pattern):
         guess = ideal_plays[opponent_history[-1]]
-    
+    elif len(opponent_history) > 5 and (opponent_history[1:6] == abbey_pattern):
+        if 6 <= len(opponent_history) <= 27:
+            pattern_list = list("RSSPPPRPRRSPSPPRPSRRSS")
+            guess = ideal_plays[pattern_list[(len(opponent_history) - 6)]]
+        else:
+            pattern_list = list("PPRPSRRSS")
+            guess = ideal_plays[pattern_list[(len(opponent_history) - 28) % len(pattern_list)]]
+    else:
+        guess = moves[(len(opponent_history) - 1) % 3]
+
 
     return guess
